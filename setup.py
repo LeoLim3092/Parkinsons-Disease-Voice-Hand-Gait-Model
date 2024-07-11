09437c0cc2d82a26c67d5814f842b5ac5427b3f8
leolim3092@ntu.edu.tw <leolim3092@ntu.edu.tw>
2024-07-11 16:15:33 +0800

    Updata setup.y for releasing on PIP pdmodel 0.0.2 and .gitignore

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pd_model',
-    version='0.1',
+    name='pdmodel',
+    version='0.0.2',
     author='leolim3092',
     author_email='your@email.com',
     description='An AI tool for screening Parkinson\'s disease',
     packages=find_packages(include=['pdmodel', 'pd_model.*']),  # Automatically discover and include all Python packages in your project
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: Apache License 2.0',
-        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
     ],
     install_requires=[
         # List your package dependencies here
